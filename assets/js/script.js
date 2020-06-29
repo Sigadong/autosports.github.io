3c261c2fb678a57b254c24831653ad182c807fa9
Sigadong <irwanto.regar30@gmail.com>
2020-06-30 01:06:43 +0700

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+
+const menuicon = document.querySelector('.menu-icon');
+
+menuicon.addEventListener('click', function(){
+  const nav = document.querySelector('.nav');
+  nav.classList.toggle('navbar');
+});
